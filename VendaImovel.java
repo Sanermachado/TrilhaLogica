a4b64cc68c9a1636b3d7177108c90b47e308b0fa
saner.machado <sanermachado@hotmail.com>
2023-09-04 14:41:48 -0300

    desafio 5

---
+++ VendaImovel.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class VendaImovel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do vendedor: ");
+        String nomeVendedor = scanner.nextLine();
+
+        System.out.print("Digite o valor do imóvel: ");
+        double valorImovel = scanner.nextDouble();
+
+        scanner.close();
+
+        // Calcula desconto
+        double comissao;
+        if (valorImovel >= 50000.00) {
+            comissao = valorImovel * 0.20;
+        } else if (valorImovel >= 30000.00) {
+            comissao = valorImovel * 0.15;
+        } else {
+            comissao = valorImovel * 0.10;
+        }
+
+        //tela
+        System.out.println("Nome do vendedor: " + nomeVendedor);
+        System.out.println("Valor do imóvel: R$" + valorImovel);
+        System.out.println("Comissão do vendedor: R$" + comissao);
+    }
+}
